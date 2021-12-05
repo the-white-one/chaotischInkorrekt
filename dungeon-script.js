065dbd7b803834c09726a77d90833111d9303054
Pascal4F <ppein@e.email>
2021-12-05 16:16:24 +0100

    Update dungeon-script.js

--- dungeon-script.js
+++ dungeon-script.js
@@ -1,1 +1,7 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+// Calling bootstrapExtra will initiliaze all the "custom properties"  
+bootstrapExtra();
+
 WA.chat.sendChatMessage('Glück auf!');
+WA.chat.sendChatMessage('Hello world', 'Mr Robot');
